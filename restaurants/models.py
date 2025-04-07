b4c1cf47f52d1620d2bb945bb34d27297d212423
rkondrat <rkondrat@stevnes.edu>
2025-04-07 00:19:42 -0400

    added establishment dattabase to models.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Establishment(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+    zipcode = models.CharField(max_length=5)
+    establishmentHours = models.CharField(max_length=12)
+    website = models.URLField()
+    ## HH Details
+    happyHourStartTime = models.TimeField()
+    happyHourEndTime = models.TimeField()
+    happyHourDayofWeek = models.CharField()
+
+    def __str__(self):
+        return self.title
