82773957d2c0ef03b8f5ba6b0c77ab45bbc0b6ba
rkondrat <rkondrat@stevnes.edu>
2025-05-01 20:56:25 -0400

    added in css for log in page

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # path('', views.login, name='login'),
-   path('', views.index, name='index'),
+   # path('', views.index, name='index'),
    # path('map/', views.ViewsIndex, name='map'),
-    # path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
