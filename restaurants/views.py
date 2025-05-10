b51c60a2ebb93121ca72d74dde83403ce2763958
Jacquelynn <jwilso414@gmail.com>
2025-05-09 22:39:21 -0400

    Styling and index page added

--- restaurants/views.py
+++ restaurants/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def restaurants(request):
     return HttpResponse("Hello world!")
+
+
+def index(request):
+    return render(request, 'restaurants/index.html')
