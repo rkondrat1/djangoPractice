0284235e84d18b65df4390ae04171d2dd3834609
Rachael Kondrat <rachaelkondrat@Rachaels-MacBook-Air.local>
2025-06-27 17:51:31 -0400

    centered login and added resturant modal

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,7 @@
 from django import forms
 from users.models import Profile
 
-class UserDataForm(forms.Form):
+class LoginForm(forms.Form):
     #UserData
-    firstName = forms.CharField()
-    lastName = forms.EmailField()
     userName = forms.CharField()
     password = forms.CharField()
-    profilePicture = forms.ImageField(required=False)
-    userLocation = forms.ChoiceField(required=False)
-    listofFavorites = forms.CharField(required=False)
